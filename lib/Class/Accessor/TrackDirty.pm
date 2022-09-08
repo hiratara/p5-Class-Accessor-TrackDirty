80e760549d1ad3b8c56a0f757e4634fd88602a03
Masahiro Honma <hiratara@cpan.org>
2022-09-08 11:53:57 +0900

    0.13

--- lib/Class/Accessor/TrackDirty.pm
+++ lib/Class/Accessor/TrackDirty.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use List::MoreUtils qw(any);
 use Storable qw(dclone freeze);
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 our $RESERVED_FIELD = '_original';
 our $NEW = 'new';
